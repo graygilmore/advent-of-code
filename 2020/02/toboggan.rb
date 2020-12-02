a3cc8ae7270d737fa886236078dcf4ba028482d0
Gray Gilmore <graygilmore@gmail.com>
2020-12-01 21:13:57 -0800

    Day two part one

---
+++ 2020/02/toboggan.rb
@@ -0,0 +1,37 @@
+require 'pry'
+require "minitest/autorun"
+require 'pathname'
+
+class PartOne
+  def initialize(password_list = input)
+    @password_list = password_list
+  end
+
+  def solution
+    @password_list.select do |password_item|
+      rules, password = password_item.split(': ')
+      limit_range, required_letter = rules.split(' ')
+      limit_min, limit_max = limit_range.split('-').map(&:to_i)
+
+      password.count(required_letter) >= limit_min && password.count(required_letter) <= limit_max
+    end.count
+  end
+
+  private
+
+  def input
+    @input ||=
+      begin
+        path = File.expand_path(File.dirname(__FILE__))
+        File.read(Pathname.new(path).join("password_list.txt")).chomp.lines.map(&:chomp)
+      end
+  end
+end
+
+class TestToboggan < Minitest::Test
+  def test_part_one
+    assert_equal 2, PartOne.new(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']).solution
+  end
+end
+
+puts "Part One: #{::PartOne.new.solution}"
