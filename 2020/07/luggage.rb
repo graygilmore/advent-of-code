c47ba78368e755d0d98b53f462cc7d9576fba985
Gray Gilmore <graygilmore@gmail.com>
2020-12-06 22:08:19 -0800

    Day seven part two

--- 2020/07/luggage.rb
+++ 2020/07/luggage.rb
@@ -12,8 +12,8 @@ class PartOne
   end
 
   def bag_contains_gold?(color)
-    bag_rules[color].include?('shiny gold') || bag_rules[color].any? do |included_color|
-      bag_contains_gold?(included_color)
+    bag_rules[color]&.keys&.include?('shiny gold') || bag_rules[color]&.any? do |name, count|
+      bag_contains_gold?(name)
     end
   end
 
@@ -29,7 +29,7 @@ class PartOne
         if inner_bags == 'no other bags.'
           [
             outer_bag,
-            []
+            {}
           ]
         else
 
@@ -38,8 +38,11 @@ class PartOne
             inner_bags.split(', ').map do |bag|
               _, count, color = bag.split(/(\d) /)
 
-              color.gsub(/bags(.)?|bag(.)?/, '').strip
-            end
+              [
+                color.gsub(/bags(.)?|bag(.)?/, '').strip,
+                count.to_i
+              ]
+            end.to_h
           ]
         end
       end.to_h
@@ -48,6 +51,21 @@ class PartOne
 end
 
 class PartTwo < PartOne
+  def solution
+    @total_bags = 0
+    count_bags('shiny gold')
+    @total_bags
+  end
+
+  def count_bags(color)
+    if bag_rules[color]&.any?
+      colors = bag_rules[color]
+
+      @total_bags += colors.values.sum
+
+      colors.each { |name, count| count.times { count_bags(name) } }
+    end
+  end
 end
 
 class Test < Minitest::Test
@@ -69,5 +87,30 @@ class Test < Minitest::Test
   end
 
   def test_part_two
+    assert_equal 32, PartTwo.new(
+      <<~INPUT
+        light red bags contain 1 bright white bag, 2 muted yellow bags.
+        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+        bright white bags contain 1 shiny gold bag.
+        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+        faded blue bags contain no other bags.
+        dotted black bags contain no other bags.
+      INPUT
+    ).solution
+    assert_equal 126, PartTwo.new(
+      <<~INPUT
+        shiny gold bags contain 2 dark red bags.
+        dark red bags contain 2 dark orange bags.
+        dark orange bags contain 2 dark yellow bags.
+        dark yellow bags contain 2 dark green bags.
+        dark green bags contain 2 dark blue bags.
+        dark blue bags contain 2 dark violet bags.
+        dark violet bags contain no other bags.
+      INPUT
+    ).solution
+    assert_equal 7867, PartTwo.new.solution
   end
 end
