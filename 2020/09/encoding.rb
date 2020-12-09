ae634ab2734f5a2499bf97ad26639edd824ef120
Gray Gilmore <graygilmore@gmail.com>
2020-12-08 22:35:04 -0800

    Day nine part two
    Not a particularly nice to read implementation. Will enjoy cleaning this
    one up a bit.

--- 2020/09/encoding.rb
+++ 2020/09/encoding.rb
@@ -35,38 +35,55 @@ end
 
 class PartTwo < PartOne
   def solution
+    bar = (0..numbers_to_search.length).to_a.combination(2).map{|i,j| numbers_to_search[i...j]}.select do |foo|
+      foo.sum == invalid_number
+    end.flatten
+
+    bar.min + bar.max
+  end
+
+  private
+
+  def numbers_to_search
+    @numbers_to_search ||= begin
+      numbers.slice(0, numbers.find_index(invalid_number))
+    end
   end
 end
 
 class Test < Minitest::Test
   def test_part_one
-    assert_equal 127, PartOne.new(
-      <<~INPUT,
-        35
-        20
-        15
-        25
-        47
-        40
-        62
-        55
-        65
-        95
-        102
-        117
-        150
-        182
-        127
-        219
-        299
-        277
-        309
-        576
-      INPUT
-    5).solution
+    assert_equal 127, PartOne.new(test_input, 5).solution
     assert_equal 177777905, PartOne.new.solution
   end
 
   def test_part_two
+    assert_equal 62, PartTwo.new(test_input, 5).solution
+    assert_equal 23463012, PartTwo.new.solution
+  end
+
+  def test_input
+    <<~INPUT
+      35
+      20
+      15
+      25
+      47
+      40
+      62
+      55
+      65
+      95
+      102
+      117
+      150
+      182
+      127
+      219
+      299
+      277
+      309
+      576
+    INPUT
   end
 end
