1cf716ff0eae40199a5d3b700d778ccb2bab09f5
Gray Gilmore <graygilmore@gmail.com>
2020-12-08 16:56:33 -0800

    Add a Computer class from day eight part one
    There's a high probability that this is going to be reused in later days
    so let's clean up the original code that we wrote and pull it out into a
    separate class.

---
+++ computer.rb
@@ -0,0 +1,70 @@
+require './base'
+
+class Computer
+  def initialize(program)
+    @program = formatted_program(program)
+  end
+
+  def run
+    run_instruction(0, 0, Set[])
+  end
+
+  private
+
+  attr_reader :program
+
+  def run_instruction(index, accumulator, completed_instructions)
+    action, value = program[index]
+
+    if index == program.length
+      # This means that there are no more instructions to run and we've
+      # successfully completed the program
+      return accumulator
+    end
+
+    if completed_instructions.add?(index)
+      case action
+      when 'nop'
+        run_instruction(index + 1, accumulator, completed_instructions)
+      when 'acc'
+        run_instruction(index + 1, accumulator + value, completed_instructions)
+      when 'jmp'
+        run_instruction(index + value, accumulator, completed_instructions)
+      end
+    else
+      # We've already hit this instruction so we're going to return the
+      # accumulator instead of running an infinite loop
+      return accumulator
+    end
+  end
+
+  def formatted_program(raw_program)
+    case raw_program
+    when String
+      raw_program.split(/\n/).map do |line|
+        action, value = line.split(' ')
+        [action, value.to_i]
+      end
+    else
+      raw_program
+    end
+  end
+end
+
+class Test < Minitest::Test
+  def test_computer
+    assert_equal 5, Computer.new(
+      <<~INPUT
+        nop +0
+        acc +1
+        jmp +4
+        acc +3
+        jmp -3
+        acc -99
+        acc +1
+        jmp -4
+        acc +6
+      INPUT
+    ).run
+  end
+end
