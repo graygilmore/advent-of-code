5054aad3a54a2373b259cd29c92b383aea4f38d4
Gray Gilmore <graygilmore@gmail.com>
2020-12-14 17:17:11 -0800

    Day thirteen part two via Jared

--- 2020/13/shuttle.rb
+++ 2020/13/shuttle.rb
@@ -27,7 +27,25 @@ class PartOne
 end
 
 class PartTwo < PartOne
+  # Thank you Jared for this one!
   def solution
+    buses_seen = []
+    step = 1
+    index = 0
+    busses.each do |next_bus|
+      buses_seen << next_bus
+      until buses_seen.all? { |bus, offset| (index + offset) % bus == 0 }
+        index += step
+      end
+      step = step * next_bus[0]
+    end
+    index
+  end
+
+  private
+
+  def busses
+    @busses ||= input.split[1].split(',').map(&:to_i).each_with_index.reject { |x,i| x.zero? }
   end
 end
 
@@ -38,6 +56,38 @@ class Test < Minitest::Test
   end
 
   def test_part_two
+    assert_equal 1068781, PartTwo.new(input).solution
+    assert_equal 3417, PartTwo.new(
+      <<~INPUT
+        939
+        17,x,13,19
+      INPUT
+    ).solution
+    assert_equal 754018, PartTwo.new(
+      <<~INPUT
+        939
+        67,7,59,61
+      INPUT
+    ).solution
+    assert_equal 779210, PartTwo.new(
+      <<~INPUT
+        939
+        67,x,7,59,61
+      INPUT
+    ).solution
+    assert_equal 1261476, PartTwo.new(
+      <<~INPUT
+        939
+        67,7,x,59,61
+      INPUT
+    ).solution
+    assert_equal 1202161486, PartTwo.new(
+      <<~INPUT
+        939
+        1789,37,47,1889
+      INPUT
+    ).solution
+    assert_equal 800177252346225, PartTwo.new.solution
   end
 
   def input
