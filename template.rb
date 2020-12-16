93d3eb963a870a46251d6d098993934bda280be0
Gray Gilmore <graygilmore@gmail.com>
2020-12-15 20:55:34 -0800

    Add a template to copy each time

---
+++ template.rb
@@ -0,0 +1,38 @@
+require './base'
+
+class PartOne
+  def initialize(input = Base.raw_input('2020/##/input.txt'))
+    @input = input
+  end
+
+  def solution
+    0
+  end
+
+  private
+
+  attr_reader :input
+end
+
+class PartTwo < PartOne
+  def solution
+    0
+  end
+end
+
+class Test < Minitest::Test
+  def test_part_one
+    assert_equal 0, PartOne.new(input).solution
+    assert_equal 0, PartOne.new.solution
+  end
+
+  def test_part_two
+    assert_equal 0, PartTwo.new(input).solution
+    assert_equal 0, PartTwo.new.solution
+  end
+
+  def input
+    <<~INPUT
+    INPUT
+  end
+end
