79768ca2555b9b97b023a0ce3068f42d77b47a23
Gray Gilmore <graygilmore@gmail.com>
2020-12-16 18:21:55 -0800

    Day sixteen part two
    A whole bunch of very likely hard to read code for this one.

--- 2020/16/tickets.rb
+++ 2020/16/tickets.rb
@@ -6,9 +6,7 @@ class PartOne
   end
 
   def solution
-    ticket_values.select do |value|
-      rule_ranges.none? { |range| range.include?(value) }
-    end.sum
+    invalid_ticket_values.sum
   end
 
   private
@@ -29,11 +27,81 @@ class PartOne
       input.split("nearby tickets:")[1].split(/,|\n/).map(&:to_i)
     end
   end
+
+  def invalid_ticket_values
+    ticket_values.select do |value|
+      rule_ranges.none? { |range| range.include?(value) }
+    end
+  end
 end
 
 class PartTwo < PartOne
   def solution
-    0
+    @possible_ticket_meanings = (0..tickets.first.count-1).map do |i|
+      [i, rules.keys]
+    end.to_h
+
+    until @possible_ticket_meanings.all? { |i, possibilities| possibilities.count == 1 } do
+      tickets.each do |ticket|
+        @possible_ticket_meanings.reject { |i,p| p.count == 1}.each do |index, p|
+          p.reject! { |name| rules[name].none? { |r| r.include?(ticket[index]) } }
+
+          if p.count == 1
+            remove_found_meaning(p.first)
+          end
+        end
+      end
+    end
+
+    @possible_ticket_meanings.select { |i, vs| vs.first.include?("departure") }.keys.map do |f|
+      my_ticket[f]
+    end.inject(:*)
+  end
+
+  private
+
+  def remove_found_meaning(name)
+    @possible_ticket_meanings.each do |index, possibilities|
+      next if possibilities.count == 1 && possibilities.first == name
+
+      if possibilities.count > 1
+        possibilities.delete(name)
+
+        if possibilities.count == 1
+          remove_found_meaning(possibilities.first)
+        end
+      end
+    end
+  end
+
+  def my_ticket
+    @my_ticket ||= input.match(/your ticket:\n(\S+)/)[1].split(',').map(&:to_i)
+  end
+
+  def rules
+    @rules ||= begin
+      input.split(/\n\nyour ticket:/)[0].split(/\n/).map do |rule|
+        name, values = rule.split(': ')
+
+        [
+          name,
+          values.scan(/\d+\-\d+/).map do |value|
+            first, last = value.split('-')
+            (first.to_i..last.to_i)
+          end
+        ]
+      end.to_h
+    end
+  end
+
+  def tickets
+    @tickets ||= begin
+      input.split("nearby tickets:")[1].split.map do |ticket|
+        ticket.split(',').map(&:to_i)
+      end.reject do |ticket|
+        ticket.any? { |ticket_value| invalid_ticket_values.include?(ticket_value) }
+      end
+    end
   end
 end
 
@@ -44,8 +112,7 @@ class Test < Minitest::Test
   end
 
   def test_part_two
-    assert_equal 0, PartTwo.new(input).solution
-    assert_equal 0, PartTwo.new.solution
+    assert_equal 953713095011, PartTwo.new.solution
   end
 
   def input
