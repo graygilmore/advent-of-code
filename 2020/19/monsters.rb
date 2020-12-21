7b85d2173a41a15c11256c8e80cbddf4eee53d5c
Gray Gilmore <graygilmore@gmail.com>
2020-12-21 11:54:14 -0800

    Day 19 part two
    Big thanks to Jared's stream for this one! His googling was much better
    than mine in order to find how recursive regexes work in Ruby.

--- 2020/19/monsters.rb
+++ 2020/19/monsters.rb
@@ -41,7 +41,11 @@ end
 
 class PartTwo < PartOne
   def solution
-    0
+    rules["8"] = "(42)+"
+    rules["11"] = "(42 31|(?<re>(42)\\g<re>?(31))+)"
+
+    regex = build_regex(rules["0"]).tr(' ', '')
+    messages.count { |message| message.match?(/\A#{regex}\z/) }
   end
 end
 
@@ -52,8 +56,9 @@ class Test < Minitest::Test
   end
 
   def test_part_two
-    assert_equal 0, PartTwo.new(input).solution
-    assert_equal 0, PartTwo.new.solution
+    assert_equal 3, PartOne.new(input_two).solution
+    assert_equal 12, PartTwo.new(input_two).solution
+    assert_equal 341, PartTwo.new.solution
   end
 
   def input
@@ -70,6 +75,58 @@ class Test < Minitest::Test
       abbbab
       aaabbb
       aaaabbb
+    INPUT
+  end
+
+  def input_two
+    <<~INPUT
+      42: 9 14 | 10 1
+      9: 14 27 | 1 26
+      10: 23 14 | 28 1
+      1: "a"
+      11: 42 31
+      5: 1 14 | 15 1
+      19: 14 1 | 14 14
+      12: 24 14 | 19 1
+      16: 15 1 | 14 14
+      31: 14 17 | 1 13
+      6: 14 14 | 1 14
+      2: 1 24 | 14 4
+      0: 8 11
+      13: 14 3 | 1 12
+      15: 1 | 14
+      17: 14 2 | 1 7
+      23: 25 1 | 22 14
+      28: 16 1
+      4: 1 1
+      20: 14 14 | 1 15
+      3: 5 14 | 16 1
+      27: 1 6 | 14 18
+      14: "b"
+      21: 14 1 | 1 14
+      25: 1 1 | 1 14
+      22: 14 14
+      8: 42
+      26: 14 22 | 1 20
+      18: 15 15
+      7: 14 5 | 1 21
+      24: 14 1
+
+      abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+      bbabbbbaabaabba
+      babbbbaabbbbbabbbbbbaabaaabaaa
+      aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+      bbbbbbbaaaabbbbaaabbabaaa
+      bbbababbbbaaaaaaaabbababaaababaabab
+      ababaaaaaabaaab
+      ababaaaaabbbaba
+      baabbaaaabbaaaababbaababb
+      abbbbabbbbaaaababbbbbbaaaababb
+      aaaaabbaabaaaaababaa
+      aaaabbaaaabbaaa
+      aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+      babaaabbbaaabaababbaabababaaab
+      aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
     INPUT
   end
 end
