40c18fc3c1160ce3073c9a76f554f6d289c0b6db
Gray Gilmore <graygilmore@gmail.com>
2021-12-04 10:42:12 -0800

    Remove curl to adventofcode.com

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,7 +6,7 @@ mkdir 2021/$day
 cp template.rb 2021/$day/program.rb
 sed -i "s/#DAY#/$day/g" 2021/$day/program.rb
 
-curl https://adventofcode.com/2021/day/$1/input --output 2021/$day/input.txt
+touch 2021/$day/input.txt
 
 bundle exec ruby 2021/$day/program.rb
 
