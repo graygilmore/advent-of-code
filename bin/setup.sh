5c8a7da05a4e8d651230cd9b72533235e1c0def7
Gray Gilmore <graygilmore@gmail.com>
2022-12-05 18:16:55 -0800

    Update setup script to 2022

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,12 +2,12 @@
 
 printf -v day "%02d" $1
 
-mkdir 2021/$day
-cp template.rb 2021/$day/program.rb
-sed -i "s/#DAY#/$day/g" 2021/$day/program.rb
+mkdir 2022/$day
+cp template.rb 2022/$day/program.rb
+sed -i "s/#DAY#/$day/g" 2022/$day/program.rb
 
-touch 2021/$day/input.txt
+touch 2022/$day/input.txt
 
-bundle exec ruby 2021/$day/program.rb
+bundle exec ruby 2022/$day/program.rb
 
 exit 0
