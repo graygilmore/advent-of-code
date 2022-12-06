f34141731494ed3fd5dcfd16b9a73845f4f5fdeb
Gray Gilmore <graygilmore@gmail.com>
2022-12-05 18:48:19 -0800

    Day 2, part 2

--- 2022/02/program.rb
+++ 2022/02/program.rb
@@ -1,24 +1,6 @@
 require './base'
 
 class PartOne
-  GAMES = {
-    "A": { # rock
-      "X": 1 + 3,
-      "Y": 2 + 6,
-      "Z": 3,
-    },
-    "B": { # paper
-      "X": 1,
-      "Y": 2 + 3,
-      "Z": 3 + 6,
-    },
-    "C": { # scissors
-      "X": 1 + 6,
-      "Y": 2,
-      "Z": 3 + 3,
-    },
-  }
-
   def initialize(input = Base.raw_input('2022/02/input.txt'))
     @input = input
   end
@@ -35,14 +17,56 @@ class PartOne
     input.split("\n").map do |round|
       opponent, me = round.split(" ")
 
-      GAMES[opponent.to_sym][me.to_sym]
+      games[opponent.to_sym][me.to_sym]
     end
+  end
+
+  def games
+    {
+      "A": { # rock
+        "X": 1 + 3,
+        "Y": 2 + 6,
+        "Z": 3,
+      },
+      "B": { # paper
+        "X": 1,
+        "Y": 2 + 3,
+        "Z": 3 + 6,
+      },
+      "C": { # scissors
+        "X": 1 + 6,
+        "Y": 2,
+        "Z": 3 + 3,
+      },
+    }
   end
 end
 
 class PartTwo < PartOne
   def solution
-    0
+    rounds.sum
+  end
+
+  private
+
+  def games
+    {
+      "A": { # rock
+        "X": 3, # lose with scissors
+        "Y": 1 + 3, # draw with rock
+        "Z": 2 + 6, # win with paper
+      },
+      "B": { # paper
+        "X": 1, # lose with rock
+        "Y": 2 + 3, # draw with paper
+        "Z": 3 + 6, # win with scissors
+      },
+      "C": { # scissors
+        "X": 2, # lose with paper
+        "Y": 3 + 3, # draw with scissors
+        "Z": 1 + 6, # win with rock
+      },
+    }
   end
 end
 
@@ -53,8 +77,8 @@ class Test < Minitest::Test
   end
 
   def test_part_two
-    assert_equal 0, PartTwo.new(input).solution
-    assert_equal 0, PartTwo.new.solution
+    assert_equal 12, PartTwo.new(input).solution
+    assert_equal 13193, PartTwo.new.solution
   end
 
   def input
