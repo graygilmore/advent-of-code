a48f19637dfd0aaa2219560198d6af7fc44dfe12
Gray Gilmore <graygilmore@gmail.com>
2022-12-11 11:47:39 -0800

    Day 6, merge parts into one class

--- 2022/06/program.rb
+++ 2022/06/program.rb
@@ -1,34 +1,21 @@
 require './base'
 
 class PartOne
-  def initialize(input = Base.raw_input('2022/06/input.txt'))
+  def initialize(input = Base.raw_input('2022/06/input.txt'), count: 4)
     @input = input
+    @count = count
   end
 
   def solution
-    input.index(marker) + 4
+    input.index(marker) + count
   end
 
   private
 
-  attr_reader :input
+  attr_reader :input, :count
 
   def marker
-    input.chars.each_cons(4).detect do |x|
-      x.uniq.size === x.size
-    end.join("")
-  end
-end
-
-class PartTwo < PartOne
-  def solution
-    input.index(marker) + 14
-  end
-
-  private
-
-  def marker
-    input.chars.each_cons(14).detect do |x|
+    input.chars.each_cons(count).detect do |x|
       x.uniq.size === x.size
     end.join("")
   end
@@ -45,11 +32,11 @@ class Test < Minitest::Test
   end
 
   def test_part_two
-    assert_equal 19, PartTwo.new("mjqjpqmgbljsphdztnvjfqwrcgsmlb").solution
-    assert_equal 23, PartTwo.new("bvwbjplbgvbhsrlpgdmjqwftvncz").solution
-    assert_equal 23, PartTwo.new("nppdvjthqldpwncqszvftbrmjlhg").solution
-    assert_equal 29, PartTwo.new("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").solution
-    assert_equal 26, PartTwo.new("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").solution
-    assert_equal 3380, PartTwo.new.solution
+    assert_equal 19, PartOne.new("mjqjpqmgbljsphdztnvjfqwrcgsmlb", count: 14).solution
+    assert_equal 23, PartOne.new("bvwbjplbgvbhsrlpgdmjqwftvncz", count: 14).solution
+    assert_equal 23, PartOne.new("nppdvjthqldpwncqszvftbrmjlhg", count: 14).solution
+    assert_equal 29, PartOne.new("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", count: 14).solution
+    assert_equal 26, PartOne.new("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", count: 14).solution
+    assert_equal 3380, PartOne.new(count: 14).solution
   end
 end
