cbf0b3027613f729d35a6c795fb8e6001e453099
Gray Gilmore <graygilmore@gmail.com>
2022-12-17 16:12:44 -0800

    Day 9, part 2
    Completely rewrote everything after realizing that I hadn't really
    interpreted the instructions properly but got lucky on part 1.

--- 2022/09/program.rb
+++ 2022/09/program.rb
@@ -1,11 +1,13 @@
 require './base'
 
 class PartOne
-  def initialize(input = Base.raw_input('2022/09/input.txt'))
+  def initialize(input = Base.raw_input('2022/09/input.txt'), total_knots: 2)
     @input = input
-    @head_position = [0,0]
-    @tail_position = [0,0]
-    @visited = {}
+    @knots = (0..total_knots-1).to_a
+    @knot_positions = @knots.map do |k|
+      [k, [0,0]]
+    end.to_h
+    @visited = [[0,0]]
   end
 
   def solution
@@ -13,94 +15,86 @@ class PartOne
       dir, length = round.split(" ")
 
       length.to_i.times do |i|
-        hx, hy = @head_position
+        move_head(dir)
 
-        case dir
-        when "U"
-          @head_position = [hx, hy + 1]
-        when "R"
-          @head_position = [hx + 1, hy]
-        when "D"
-          @head_position = [hx, hy - 1]
-        when "L"
-          @head_position = [hx - 1, hy]
-        end
-
-        move_tail(dir) unless touching?
-
-        if @visited[@tail_position]
-          @visited[@tail_position] += 1
+        if !touching?(0, 1)
+          move_next_knot(dir, 1)
         else
-          @visited[@tail_position] = 1
+          @visited << knot_positions[knot_positions.keys.last]
         end
       end
     end
 
-    @visited.keys.size
+    @visited.uniq.size
   end
 
   private
 
-  attr_reader :input
+  attr_reader :input, :knot_positions
 
-  def touching?
-    tx, ty = @tail_position
+  def move_head(dir)
+    hx, hy = knot_positions[0]
 
-    (tx-1..tx+1).to_a.any? do |x|
-      (ty-1..ty+1).to_a.any? do |y|
-        [x,y] == @head_position
+    case dir
+    when "U"
+      knot_positions[0] = [hx, hy + 1]
+    when "R"
+      knot_positions[0] = [hx + 1, hy]
+    when "D"
+      knot_positions[0] = [hx, hy - 1]
+    when "L"
+      knot_positions[0] = [hx - 1, hy]
+    end
+  end
+
+  def touching?(k1, k2)
+    x2, y2 = knot_positions[k2]
+
+    (x2-1..x2+1).to_a.any? do |x|
+      (y2-1..y2+1).to_a.any? do |y|
+        [x,y] == knot_positions[k1]
       end
     end
   end
 
-  def move_tail(dir)
-    hx, hy = @head_position
-    tx, ty = @tail_position
+  def move_next_knot(dir, knot)
+    previous_knot = knot_positions[knot - 1]
+    current_knot = knot_positions[knot]
 
-    if (@head_position.sum - @tail_position.sum).abs == 2
-      case dir
-      when "U"
-        @tail_position = [tx, ty + 1]
-      when "R"
-        @tail_position = [tx + 1, ty]
-      when "D"
-        @tail_position = [tx, ty - 1]
-      when "L"
-        @tail_position = [tx - 1, ty]
-      end
+    hx, hy = previous_knot
+    tx, ty = current_knot
+
+    x_modifier = hx > tx ? 1 : -1
+    y_modifier = hy > ty ? 1 : -1
+
+    if hy == ty
+      knot_positions[knot] = [tx + x_modifier, ty]
+    elsif hx == tx
+      knot_positions[knot] = [tx, ty + y_modifier]
     else
-      case dir
-      when "U"
-        @tail_position = hx > tx ? [tx + 1, ty + 1] : [tx - 1, ty + 1]
-      when "R"
-        @tail_position = hy > ty ? [tx + 1, ty + 1] : [tx + 1, ty - 1]
-      when "D"
-        @tail_position = hx > tx ? [tx + 1, ty - 1] : [tx - 1, ty - 1]
-      when "L"
-        @tail_position = hy > ty ? [tx - 1, ty + 1] : [tx - 1, ty - 1]
-      end
+      knot_positions[knot] = [tx + x_modifier, ty + y_modifier]
     end
-  end
-end
 
-class PartTwo < PartOne
-  def solution
-    0
+    if knot == knot_positions.keys.last
+      @visited << knot_positions[knot]
+    else
+      move_next_knot(dir, knot + 1) unless touching?(knot, knot + 1)
+    end
   end
 end
 
 class Test < Minitest::Test
   def test_part_one
-    assert_equal 13, PartOne.new(input).solution
+    assert_equal 13, PartOne.new(input_simple).solution
     assert_equal 6023, PartOne.new.solution
   end
 
   def test_part_two
-    assert_equal 0, PartTwo.new(input).solution
-    assert_equal 0, PartTwo.new.solution
+    assert_equal 36, PartOne.new(input_long, total_knots: 10).solution
+    assert_equal 2533, PartOne.new(total_knots: 10).solution
   end
 
-  def input
+  def input_simple
     <<~INPUT
       R 4
       U 4
@@ -110,6 +104,19 @@ class Test < Minitest::Test
       D 1
       L 5
       R 2
+    INPUT
+  end
+
+  def input_long
+    <<~INPUT
+      R 5
+      U 8
+      L 8
+      D 3
+      R 17
+      D 10
+      L 25
+      U 20
     INPUT
   end
 end
